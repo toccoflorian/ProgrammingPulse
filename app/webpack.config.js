871d18ddc52f2a55926ab0dde8bf61c4afc9c361
Florian <t.florian181181@gmail.com>
2023-11-05 19:34:02 +0100

    05/11/23

---
+++ app/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const cleanWebpackPlugin = require("clean-webpack-plugin");
+
+module.exports = {
+    entry: {
+        main: path.join(__dirname, "src/index.js"), // prend en entrée index.js et dependances pour creer le bundle 'main'
+        commun: path.join(__dirname, "src/commun.js"),
+    },
+    output: {
+        path: path.join(__dirname, "dist"),
+        filename: "[name].bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /(node_modules)/,
+                use: ["babel-loader"]
+            },
+
+            {
+                test: /\.scss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new cleanWebpackPlugin.CleanWebpackPlugin(),
+        // new copyWebpackPlugin({ // le copy-webpack-plugin va copier des élement dans le dist (images par exemple)
+        //     patterns: [
+        //         {
+        //             from: "./assets/*", // d'ou on copie le webpack plugin
+        //             to: "", // la ou on le met
+        //             // flatten: true // pour que le chemin soit le 'to' et non le 'to' + 'from'
+        //         },
+
+        //     ]
+        // }),
+
+        new HtmlWebpackPlugin({
+            filename: 'index.html', // nom du fichier dans le dist
+            template: path.join(__dirname, "./src/index.html"), // chemin du template (fichier html)
+            chunks: ["main", "commun"] // liste des scripts inserer dans les fichiers html
+        }),
+
+
+
+
+    ],
+    stats: "minimal",
+    devtool: "source-map",
+    mode: "development",
+    devServer: {
+        open: true,
+        static: path.resolve(__dirname, './dist'),
+        watchFiles: ['./src/**'],
+        port: 4000,
+        hot: true,
+    }
+};
