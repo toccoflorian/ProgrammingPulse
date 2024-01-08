06b5923afea205284f1dde9b8a240ab0de69248a
Florian <t.florian181181@gmail.com>
2024-01-08 17:35:18 +0100

    script database

--- back_end/init_database.py
+++ back_end/init_database.py
@@ -25,10 +25,10 @@ print()
 for i, column in enumerate(columns):
     print()
     if i == 0:
-        sql_request = f"CREATE TABLE users ({column[0]} {column[1]} );"
+        sql_request = f"CREATE TABLE users ({column[0]} {column[1]} )"
         print(sql_request)
     else:
-        sql_request = f"ALTER TABLE users ADD {column[0]} {column[1]});"
+        sql_request = f"ALTER TABLE users ADD {column[0]} {column[1]})"
     cursor.execute(str(sql_request))
 # sql_request = f"ALTER TABLE `programmingpulsestudio`.`users` PRIMARY KEY (id);"
 
