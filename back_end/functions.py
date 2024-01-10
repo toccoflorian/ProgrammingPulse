3fbeff81fb780e7df4b825e2eb597bbaabb184d0
Florian <t.florian181181@gmail.com>
2024-01-10 14:22:40 +0100

    demande d'un chiffre, d'une majuscule et d'un caractère special pour le mot de passe

--- back_end/functions.py
+++ back_end/functions.py
@@ -2,24 +2,40 @@ import json
 import bleach
 import re
 
+def maj_in_pwd(pwd, nb_maj):
+    majs = 0
+    for letter in pwd:
+        majs += 1 if letter.isupper() else 0
+    return True if majs >= nb_maj else False
+
+def numeric_in_pwd(pwd, nb_numeric):
+    spec_chars = 0
+    for letter in pwd:
+        spec_chars += 1 if letter.isnumeric() else 0
+    return True if spec_chars >= nb_numeric else False
+
+def special_char_in_pwd(pwd, nb_special_char):
+    special_chars = 0
+    for letter in pwd:
+        special_chars += 1 if re.search(r'[^A-Za-z0-9]', letter) else 0
+    return True if special_chars >= nb_special_char else False
+
 
 def sanitise_data(data) -> any:
-    def password_and_confirm_password_are_similar(passwords) -> bool:
-        print('passwords[0] == passwords[1]', passwords[0] == passwords[1])
-        if passwords[0] == passwords[1]:
-            return True
-        return False
 
     clean_data = {}
 
     for key, value in data.items():
-        if key == "passwords":
-            print("value", value)
-            if len(value[0]) < 4 or len(value[1]) < 4:
-                return False, "Le mot de passe et la confirmation doivent contenir entre 4 et 45 caractères."
-            if not password_and_confirm_password_are_similar(value):
-                return False, "Le mot de passe et la confirmation ne sont pas identiques."
-            clean_data["password"] = value[0]
+        if key == "password":
+            if len(value) < 8 or len(value) > 45:
+                return False, "Le mot de passe et la confirmation doivent contenir entre 8 et 45 caractères."
+            if not special_char_in_pwd(value, 1):
+                return False, "Votre mot de passe doit contenir au moins 1 caractère special."
+            if not numeric_in_pwd(value, 1):
+                return False, "Votre mot de passe doit contenir au moins 1 chiffre."
+            if not maj_in_pwd(value, 1):
+                return False, "Votre mot de passe doit contenir au moins 1 majuscule."
+            clean_data[key] = value
 
         elif key == "mail":           # sanitise e-mail
             if not re.match(r"[^@]+@[^@]+\.[^@]+", value) and len(value) <= 45:
@@ -31,15 +47,24 @@ def sanitise_data(data) -> any:
                 return False, "Numéro de téléphone incorrect."
             clean_data[key] = value
 
-        elif key in {"familyname", "givenname"} and 3 > len(value) > 45:
-            return False, f"Le champ {key} doit contenir 3 caractères au minimum."
+        elif key == "familyname" or key == "givenname":
+            if 3 > len(value) > 45:
+                return False, f"Le champ {key} doit contenir 3 et 45 caractères."
+            clean_data[key] = bleach.clean(value)
 
         elif key == "comment":
             clean_data[key] = bleach.clean(value)
 
+        elif key == "message":
+            if 45 <= len(value) <= 3000:
+                return False, f"Le champ {key} doit contenir 45 et 3000 caractères."
+            clean_data[key] = bleach.clean(value)
+
         elif isinstance(value, str):            # sanitise chaînes de caractères
-            if not len(value) < 45 and key != "message":
+            if len(value) > 45 and key != "message":
                 return False, f"Le longueur du champ '{key}' est trop grande."
             clean_data[key] = bleach.clean(value)
+        else:
+            return False, "Une donnée n'as pas pu être traitée."
 
     return True, clean_data
