51ca6bb1441bc58084cdfff48a0dddfd7c6ec1f9
Florian <t.florian181181@gmail.com>
2024-01-14 23:50:44 +0100

    14/01/24

--- back_end/security/mail_connection_manager.py
+++ back_end/security/mail_connection_manager.py
@@ -1,7 +1,7 @@
 import os, dotenv
 def get_mail_credentials():
     dotenv.load_dotenv()
-    return {"mail": os.getenv("MAIL"), "password": os.getenv("MAIL_PASSWORD")}
+    return {"mail": os.getenv("MAIL_IDENTIFIANT"), "password": os.getenv("MAIL_PASSWORD")}
 
 ###########################################################################################
 #       refactoring pour ne pas retourner de credentials
