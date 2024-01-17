284f99a767bd6928962de92fde2c87cc21d033c8
Florian <t.florian181181@gmail.com>
2024-01-17 16:46:03 +0100

    17/01/24 test serveur

--- front_end/src/pages/NotFoundPage/NotFoundPage.jsx
+++ front_end/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
 
 
 export default function NotFoundPage() {
     return (<>
         <h1>Erreur page introuvable</h1>
+        <Link to="/HomePage">Page d&#39;acceuil</Link>
     </>)
 }
