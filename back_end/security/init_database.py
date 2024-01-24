6dee7c48708f44893f8650ef049bed1a5bbc4674
Florian <t.florian181181@gmail.com>
2024-01-24 14:30:32 +0100

    config serveur - 24/01/24

--- back_end/security/init_database.py
+++ back_end/security/init_database.py
@@ -62,28 +62,26 @@ tables = {
     
 }
 
+def create_tables(tables):
+    print()
+    for table_name in tables:
+        print("\ncréation de la table " + table_name)
+        try:
+            sql_request = f"CREATE TABLE {table_name} (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))"
+            cursor.execute(str(sql_request))
+        except:
+            pass
+        print(table_name, "créer !\n")
 
-print()
-for table_name in tables:
-    print("\ncréation de la table " + table_name)
-    try:
-        sql_request = f"CREATE TABLE {table_name} (id INT NOT NULL AUTO_INCREMENT, PRIMARY KEY (id))"
-        cursor.execute(str(sql_request))
-    except:
-        pass
-    print(table_name, "créer !\n")
-
-    for column in tables[table_name]:
-        print("création de la colonne " + column[0] + "-" + column[1])
-        sql_request = f"ALTER TABLE {table_name} ADD {column[0]} {column[1]}"
-        print("colonne " + column[0] + " créer !")
-        print()
-        cursor.execute(str(sql_request))
+        for column in tables[table_name]:
+            print("création de la colonne " + column[0] + "-" + column[1])
+            sql_request = f"ALTER TABLE {table_name} ADD {column[0]} {column[1]}"
+            print("colonne " + column[0] + " créer !")
+            print()
+            cursor.execute(str(sql_request))
+        
+        print("Table " + table_name + " terminée !\n")
     
-    print("Table " + table_name + " terminée !\n")
-    
-
-print("Script terminé avec succès !! \n")
 
 
 
@@ -91,44 +89,47 @@ print("Script terminé avec succès !! \n")
 DB.close_connection(connexion, cursor)
 
 
-# import subprocess
+import subprocess
+import os
+import dotenv
 
-# # Fonction pour exécuter une commande et retourner la sortie
-# def run_command(command):
-#     process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-#     return process.stdout, process.stderr, process.returncode
+dotenv.load_dotenv()
+# Fonction pour exécuter une commande et retourner la sortie
+def run_command(command):
+    process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    return process.stdout, process.stderr, process.returncode
 
-# # Installer MySQL Server
-# print("Installation de MySQL Server...")
-# stdout, stderr, exitcode = run_command("sudo apt-get update && sudo apt-get install -y mysql-server")
+# Installer MySQL Server
+print("Installation de MySQL Server...")
+stdout, stderr, exitcode = run_command("sudo apt-get update && sudo apt-get install -y mysql-server")
 
-# if exitcode != 0:
-#     print(f"Erreur lors de l'installation de MySQL: {stderr}")
-#     exit(1)
+if exitcode != 0:
+    print(f"Erreur lors de l'installation de MySQL: {stderr}")
+    exit(1)
 
-# # Sécuriser l'installation de MySQL
-# print("Sécurisation de MySQL...")
-# # Les commandes suivantes sont des exemples et peuvent nécessiter des ajustements
-# # Vous devriez remplacer 'your_root_password' avec le mot de passe root souhaité
-# commands = [
-#     "sudo mysql_secure_installation",
-#     "sudo mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'your_root_password';\"",
-#     "sudo mysql -e \"FLUSH PRIVILEGES;\""
-# ]
+# Sécuriser l'installation de MySQL
+print("Sécurisation de MySQL...")
+# Les commandes suivantes sont des exemples et peuvent nécessiter des ajustements
+# Vous devriez remplacer 'your_root_password' avec le mot de passe root souhaité
+commands = [
+    "sudo mysql_secure_installation",
+    f"sudo mysql -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '{os.getenv('PASSWORD')}';\"",
+    "sudo mysql -e \"FLUSH PRIVILEGES;\""
+]
 
-# for command in commands:
-#     stdout, stderr, exitcode = run_command(command)
-#     if exitcode != 0:
-#         print(f"Erreur lors de la sécurisation de MySQL: {stderr}")
-#         exit(1)
+for command in commands:
+    stdout, stderr, exitcode = run_command(command)
+    if exitcode != 0:
+        print(f"Erreur lors de la sécurisation de MySQL: {stderr}")
+        exit(1)
 
-# # Créer une base de données 'programmingpulsestudio'
-# print("Création de la base de données 'programmingpulsestudio'...")
-# stdout, stderr, exitcode = run_command("sudo mysql -u root -p your_root_password -e \"CREATE DATABASE programmingpulsestudio;\"")
+# Créer une base de données 'programmingpulsestudio'
+print("Création de la base de données 'programmingpulsestudio'...")
+stdout, stderr, exitcode = run_command(f"sudo mysql -u root -p {os.getenv('PASSWORD')} -e \"CREATE DATABASE {os.getenv('DB_NAME')};\"")
 
-# if exitcode != 0:
-#     print(f"Erreur lors de la création de la base de données: {stderr}")
-#     exit(1)
+if exitcode != 0:
+    print(f"Erreur lors de la création de la base de données: {stderr}")
+    exit(1)
 
 # # Créer une table 'users'
 # print("Création de la table 'users'...")
@@ -143,5 +144,5 @@ DB.close_connection(connexion, cursor)
 # if exitcode != 0:
 #     print(f"Erreur lors de la création de la table 'users': {stderr}")
 #     exit(1)
-
-# print("Script terminé avec succès.")
+create_tables(tables)
+print("Script terminé avec succès.")
